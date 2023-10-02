1b99c8f936db01c16fc446c3de8a62f531773f9e
Suzanne Rapin <suzanne.rapin@lilo.org>
2023-10-02 18:16:16 +0200

    intra version

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -6,7 +6,7 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 21:27:27 by srapin            #+#    #+#             */
-/*   Updated: 2023/09/27 19:18:53 by srapin           ###   ########.fr       */
+/*   Updated: 2023/09/27 20:09:49 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ int	main(int ac, char **av)
 	pid_t	*pids;
 
 	if (!check_args(ac, av))
+	{
+		printf("parse error\n");
 		return (1);
+	}
 	init(ac, av, &data);
 	pids = lets_gow(&data);
 	if (!pids)
